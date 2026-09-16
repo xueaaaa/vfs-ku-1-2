888c0626640303cf270340db163c309e256e75e2
xueaaaa <ss.bazunov@gmail.com>
2026-09-16 10:22:29 +0300

    feat(repl): commands execute

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+	"vfs/internal/command"
+	"vfs/internal/executor"
+	"vfs/internal/parser"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -16,11 +22,68 @@ func main() {
 
 	output := widget.NewMultiLineEntry()
 	output.Disable()
+	appendOutput := func(lines ...string) {
+		text := output.Text
+		for _, l := range lines {
+			text += l + "\n"
+		}
+		output.SetText(text)
+	}
+
+	command.NewCommand(
+		"ls",
+		[]string{"arg1", "arg2"},
+		func(args []string) (any, error) {
+			appendOutput(fmt.Sprintf("command = ls, args = %s", args))
+			return nil, nil
+		},
+	)
+
+	command.NewCommand(
+		"cd",
+		[]string{"arg1", "arg2"},
+		func(args []string) (any, error) {
+			appendOutput(fmt.Sprintf("command = cd, args = %s", args))
+			return nil, nil
+		},
+	)
+
+	command.NewCommand(
+		"exit",
+		[]string{},
+		func(strings []string) (any, error) {
+			a.Quit()
+			return nil, nil
+		},
+	)
 
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Enter command here...")
 
+	runCommand := func(line string) {
+		line = strings.TrimSpace(line)
+		appendOutput("> " + line)
+
+		cmd, args, err := parser.Parse(line)
+		if err != nil {
+			appendOutput(err.Error())
+			return
+		}
+
+		_, err = executor.Execute(cmd, args)
+		if err != nil {
+			appendOutput(err.Error())
+			return
+		}
+	}
+
+	input.OnSubmitted = func(text string) {
+		runCommand(text)
+		input.SetText("")
+	}
+
 	content := container.NewBorder(nil, input, nil, nil, container.NewScroll(output))
 	w.SetContent(content)
+
 	w.ShowAndRun()
 }
